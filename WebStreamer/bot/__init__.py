24fb1265ce4d7183777a8fe3aaa1de601d517f6d
scprojectslk <80474245+scprojectslk@users.noreply.github.com>
2021-06-22 21:26:10 +0530

    Update __init__.py

--- WebStreamer/bot/__init__.py
+++ WebStreamer/bot/__init__.py
@@ -1,5 +1,5 @@
 # This file is a part of TG-FileStreamBot
-# Coding : Jyothis Jayanth [@EverythingSuckz]
+# Coding : SC Projects LK [@SCProjectsLK]
 
 from pyrogram import Client
 from ..vars import Var
